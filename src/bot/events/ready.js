a5edeb016f704cc75edc2f044ec9621da6604e5f
programmer-offbeat <87474641+programmer-offbeat@users.noreply.github.com>
2021-12-17 08:29:01 +0530

    Update ready.js

--- src/bot/events/ready.js
+++ src/bot/events/ready.js
@@ -3,7 +3,7 @@ const figlet = require("figlet");
 
 module.exports = async (client) => {
   client.Ready = true;
-  client.user.setActivity("motchys ticket bot", { type: "STREAMING", url:"https://www.twitch.tv/captain_motchy" }).then(() => {
+  client.user.setActivity("tyson nab", { type: "STREAMING", url:"https://www.twitch.tv/captain_motchy" }).then(() => {
     client.Manager.init(client.user.id);
     client.log("Successfully Logged in as " + client.user.tag);
   });
